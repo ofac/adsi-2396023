76283219844226ff03ac5113d64a53b864020f6c
ofac <ofaczero@gmail.com>
2022-09-27 13:35:43 -0500

    feat: all files php and tailwind

--- 12-php/03-comments.php
+++ 12-php/03-comments.php
@@ -4,36 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=" Comments "?></title>
+    <title><?="Comments"?></title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/fonts.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
-          <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
             <a class="navbar-brand" href="index.php">PHP & Bootstrap 5</a>
-            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavId">
-                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Main Menu (PHP) </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href=""> Comments  </a>
-                    </li>
-                </ul>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.php">&larr; Main Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">03- Comments</a>
+                </li>
+            </ul>
             </div>
-      </div>
+        </div>
     </nav>
-    
     <main class="container">
         <div class="row">
             <div class="col-md-6 offset-md-3 text-center">
-                <?php echo "<h1 class='text-center my-5'> Comments </h1>"; ?>
-                <hr>
+                <?php 
+                    echo "<h1 class='mt-5'>Comments</h1><hr>";
+                ?>
                 <?php 
 				// Define the classes of button
 				$classes = "btn btn-lg btn-outline-primary";
@@ -49,6 +48,7 @@
         </div>
     </main>
 
+    <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
